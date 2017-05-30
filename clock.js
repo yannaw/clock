e095ca53efc45440c96d73262826722c067602bd
yannaw <yannaw@hotmail.com>
2017-05-30 16:15:47 +0200

    awl

---
+++ clock.js
@@ -0,0 +1,30 @@
+﻿(function() {
+    
+  var time=function(){
+  var now = new Date();
+        
+     var hourElement= document.getElementById('hours');
+     var minutesElement= document.getElementById('minutes');
+   var secondsElement= document.getElementById('seconds');
+
+    var hoursText=now.getHours();
+    var minutesText=now.getMinutes();
+    var secondsText=now.getSeconds();
+    if(secondsText<10){
+      secondsText='0'+secondsText;
+    }
+        if(minutesText<10){
+      minutesText='0'+minutesText;
+    }
+        if(hoursText<10){
+      hoursText='0'+hoursText;
+    }
+  hourElement.innerHTML=hoursText;
+   minutesElement.innerHTML=minutesText;
+     secondsElement.innerHTML=secondsText;
+    
+    }
+  time();
+  var interval = setInterval(time, 1000);
+  
+})();
---
+++ script.js
@@ -0,0 +1,45 @@
+﻿document.write("Klart");    
+  var doctitle= document.getElementById('doctitle');
+  console.log(doctitle.innerHTML);
+  doctitle.innerHTML="Tjena";
+
+  var addDot = function(){
+    this.innerHTML = this.innerHTML +".";
+    
+  }
+ addDot(); 
+  
+  doctitle.addEventListener('click', addDot,false);
+  
+  
+  var elements= document.getElementsByClassName('ingress');
+  for(var i=0; i<elements.length; i++){
+    elements[i].addEventListener('click', addDot, false);
+  }
+  
+
+
+  (function () {
+
+      var time = function () {
+          var now = new Date();
+
+          document.write(now.getHours() + '.'
+                         + now.getMinutes() + ':'
+                        + now.getSeconds() + '<br />');
+
+          var hourElement = document.getElementById('hours');
+          var minutesElement = document.getElementById('minutes');
+          var secondsElement = document.getElementById('seconds');
+          var hoursText = now.getHours();
+          var minutesText = now.getMinutes();
+          var secondsText = now.getSeconds();
+          hourElement.innerHTML = hoursText;
+          minutesElement.innerHTML = hoursText;
+          secondsElement.innerHTML = hoursText;
+
+      }
+      time();
+      var interval = setInterval(time, 1000);
+
+  })();
